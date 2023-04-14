dbe3d312ea8c31b9d60027b2f07f24e77fcb0361
santiagov01 <santiago.vargas5@udea.edu.co>
2023-04-13 20:29:33 -0500

    Adicion decodificacion 1

--- Ej_Desencript/Desencript/main.cpp
+++ Ej_Desencript/Desencript/main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 using namespace std;
-int n = 4;
-int metodo = 0;
-void invertir(char[],int,int,char[],int*); //bloque y frecuencia/condicion para invertir
-int contarbins(char[],int semilla);
-void encripting(char[], int, char*, int *cont);
-int s_real(char*,int);
-void c_bin(char*,int,char*,int);
+
+int n;
+int metodo;
+void lectura(char *nf_ouput, char *nf_input, char* bin_linea, int size_cad);
+void bin_to_char(char* cadena);
+void decode(char binario[],int nBloques, char*pegar,int *cont);
+void invertir(char actual[], int freq,int semilla,char *pegar,int *cont);
+int contarbins(char anterior[], int semilla);
 void first_params(char[],char[]);
-
-
-
-
-void lectura(char *nf_ouput, char *nf_input, char* c_original, int size_cad);
-
-
+int s_real(char* cad, int s);
 int main()
 {
     char nf_input[64]=" ", nf_output[64]=" ";//nombre archivos
     first_params(nf_input,nf_output); //semilla, metodo, nombre archivos
-
-    char c_original[256] =" "; //cadena original
-
+    char bin_linea[256*8] =" "; //cadena original
 
 
-    lectura(nf_output,nf_input,c_original,sizeof(c_original));
+    lectura(nf_output,nf_input,bin_linea,sizeof(bin_linea));
 
     return 0;
+}
+int s_real(char* cad, int s){
+    /*Conoce tamaño real de la cadena*/
+    int tam_real = 0;
+    for(int i = 0; i< s;i++){
+        if(cad[i] == '\0')break;
+        tam_real++;
+    }
+    return tam_real;
 }
 void invertir(char actual[], int freq,int semilla,char *pegar,int *cont){
 
@@ -69,7 +70,8 @@ void invertir(char actual[], int freq,int semilla,char *pegar,int *cont){
         }
 
     }else{
-        for(int i = 0;i<semilla;i++){
+        for(int i = 0;i<semilla;i++){//cuando semilla = 2, freq =3
+            //Queda igual
             pegar[*cont]=actual[i];
             (*cont)++;
         }
@@ -87,8 +89,8 @@ int contarbins(char anterior[], int semilla){
     if(ceros > unos)return 2;
     else return 3;
 }
-void encripting(char binario[],int nBloques, char*pegar,int *cont){
-    char *anterior= new char [n];
+void decode(char binario[],int nBloques, char*pegar,int *cont){
+    char *anterior= new char [n]; //debe ser de la cadena a pegar
     char *actual = new char [n];
 
     int semilla = n;
@@ -96,15 +98,14 @@ void encripting(char binario[],int nBloques, char*pegar,int *cont){
     for(int z = 0; z<(nBloques*semilla);z++)pegar[z]='0'; //inicializa cadena en 0.
     //para el primer bloque
     for(int i = 0; i<semilla;i++){
-        anterior[i]=binario[i];
+        //anterior[i]=binario[i];
         if(binario[i]=='0'){
             pegar[*cont] = '1';
-
         }else{
-
             pegar[*cont]='0';
         }
         (*cont)++;//me dice en que posición va.
+        anterior[i]=pegar[i];
     }
     //para el resto de bloques
     c = *cont; //variable auxiliar
@@ -116,7 +117,7 @@ void encripting(char binario[],int nBloques, char*pegar,int *cont){
         //hacer operacion
         invertir(actual,contarbins(anterior,semilla),semilla,pegar,cont);
         for(int j=0;j<semilla;j++){
-            anterior[j]= actual[j];//actualiza anterior
+            anterior[j]= pegar[j-semilla+(*cont)];//actualiza anterior
         }
 
     }
@@ -124,8 +125,7 @@ void encripting(char binario[],int nBloques, char*pegar,int *cont){
     delete[] actual;
     delete[] anterior;
 }
-
-void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
+void lectura(char *nf_ouput,char *nf_input, char* bin_linea, int size_cad){
     ifstream fin;               //stream de entrada, lectura
     ofstream fout;              //stream de salida, escritura
     int nBloques = 0;
@@ -159,22 +159,19 @@ void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
         if(!fout.is_open()){
             throw '1';
         }
+
         if(saltos == 0){
-            fin.getline(cad_linea,256); //leer linea del archivo y guardar en variable.
-            tam_cad = s_real(cad_linea,size_cad);//longitud real de cada linea
+            fin.getline(bin_linea,256);//extrae linea a linea.
+            tam_cad = s_real(bin_linea,size_cad);//longitud real de cada linea
+
             if(tam_cad>0){
-
-                nBloques = tam_cad*8/n;
-                if(tam_cad*8%n != 0)nBloques++;//revisa division exacta
-                char pegar[nBloques*n];
                 int cont = 0;
-                char bin[nBloques*n];
-                c_bin(cad_linea,tam_cad,bin,nBloques);//convierte cade a binario
-
-
+                nBloques = tam_cad/n;
+                char pegar[nBloques*n];
+                //c_bin(cad_linea,tam_cad,bin,nBloques);
                 switch (metodo) {
                 case 1:
-                    encripting(bin,nBloques,pegar,&cont);
+                    decode(bin_linea,nBloques,pegar,&cont);
                     break;
                 case 2:
 
@@ -183,6 +180,7 @@ void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
                     cout << "Debes ingresar el método correcto. ";
                     break;
                 }
+
                 for(int z = 0; z<nBloques*n;z++){
                     fout << pegar[z];
                 }
@@ -191,19 +189,17 @@ void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
             for(int c = 0; c<=saltos;c++){
                 // REVISAR CUANDO SEMILLA > TAM CAD*8
 
-                fin.getline(cad_linea,256);//extrae linea a linea.
-                tam_cad = s_real(cad_linea,size_cad);//longitud real de cada linea
+                fin.getline(bin_linea,256);//extrae linea a linea.
+                tam_cad = s_real(bin_linea,size_cad);//longitud real de cada linea
 
                 if(tam_cad>0){
                     int cont = 0;
-                    nBloques = tam_cad*8/n;
-                    if(tam_cad*8%n != 0)nBloques++;//revisa division exacta
-                    char bin[tam_cad*8];
+                    nBloques = tam_cad/n;
                     char pegar[nBloques*n];
-                    c_bin(cad_linea,tam_cad,bin,nBloques);
+                    //c_bin(cad_linea,tam_cad,bin,nBloques);
                     switch (metodo) {
                     case 1:
-                        encripting(bin,nBloques,pegar,&cont);
+                        decode(bin_linea,nBloques,pegar,&cont);
                         break;
                     case 2:
 
@@ -223,13 +219,9 @@ void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
             }
 
         }
-
-
         fout.close();
         fin.close();
-
     }
-
     catch (char c){
         cout<<"Error # "<<c<<": ";
         if(c=='1'){
@@ -243,71 +235,9 @@ void lectura(char *nf_ouput,char *nf_input, char* cad_linea, int size_cad){
         cout<<"Error no definido\n";
     }
 
-
 }
 
 
-void regla(){
-    /*
-    semilla n; //será el tamaño del bloque
-    cadena encriptada[mismo tamaño de bin];
-
-    cadena bloque[n]; (?) usar mejor apuntadores??
-    bloque = matriz[i];
-    para primer bloque:
-    invertir(bloque,1)
-    funcion invertir(bloque, frecuencia de intercambio)
-        for(int i = 0; i<n; i+=frecuencia)
-            if( bloque[i] == '1')bloque[i] = '0'
-            else bloque[i]=='1'
-    condicion:
-    funcion contar 1s y 0s
-        ingresa bloque
-        verifica cada elemento del bloque
-        contar 1s y 0s
-      comparar y retornar segun sea el caso.
-      retornará frecuencia
-
-    invocar invertir()
-
-    */
-}
-
-int s_real(char* cad, int s){
-    /*Conoce tamaño real de la cadena*/
-    int tam_real = 0;
-    for(int i = 0; i< s;i++){
-        if(cad[i] == '\0')break;
-        tam_real++;
-    }
-    return tam_real;
-}
-void c_bin(char* cad,int tam_real,char* bin,int nBloques){
-    /*
-      *cad: cadena original
-      tam_real: tamaño real de la cadena
-      *bin: cadena para guardar binario
-    */
-    for(int i = 0; i<nBloques*n;i++){
-        bin[i]= '0';
-    }
-    int ltr =0;
-    int cont = 7; //contar en que letra voy
-    //[...,...,...]
-    for(int i = 0; i<tam_real;i++){
-        ltr = cad[i];
-        int res = 0;
-        for(int j = 0; j<8;j++){
-            res = ltr%2;
-            ltr = ltr/2;
-
-            bin[cont-j] = char(res+48);
-        }
-        cont+=8;
-    }
-
-
-}
 void first_params(char in[], char ou[]){
     /*Usuario ingresa semilla, metodo y nombres de archivos.
     in[]: nombre achivo lectura
@@ -327,4 +257,3 @@ void first_params(char in[], char ou[]){
     cin >> ou;
 
 }
-
